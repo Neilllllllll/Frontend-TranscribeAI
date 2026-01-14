82d790a324651db15809a0707fe8e8fe3335b917
Neil <neil.rabia972972@gmail.com>
2026-01-14 10:40:41 +0100

    fix des nom de variable d'env

--- src/modules/Diarization/config.ts
+++ src/modules/Diarization/config.ts
@@ -1,7 +1,7 @@
 import {DiarizationEnv} from '../../core/config/env.ts'
 
-export const MAXTIMEPROCESSING = DiarizationEnv.MAXTIMEPROCESSING || 3000000; // En millisecondes
-export const TIMEBETTWENEACHPOLLING = DiarizationEnv.TIMEBETTWENEACHPOLLING || 3000; // En millisecondes
+export const MAXTIMEPROCESSING = Number(DiarizationEnv.MAX_TIME_PROCESSING) || 3000000; // En millisecondes
+export const TIMEBETTWENEACHPOLLING = Number(DiarizationEnv.TIME_BETWEEN_EACH_POLLING) || 3000; // En millisecondes
 export const API_KEY = DiarizationEnv.API_KEY || "";
-export const MAXSIZEAUDIO = Number(DiarizationEnv.MAXSIZEAUDIO) || 25;
-export const MAXSPEAKERS = Number(DiarizationEnv.MAXSPEAKERS) || 10;
+export const MAXSIZEAUDIO = Number(DiarizationEnv.MAX_SIZE_AUDIO) || 25;
+export const MAXSPEAKERS = Number(DiarizationEnv.MAX_SPEAKERS) || 10;
--- src/modules/TranscriptionBatch/config.ts
+++ src/modules/TranscriptionBatch/config.ts
@@ -1,6 +1,6 @@
 import {BatchTranscriptionEnv} from '../../core/config/env.ts'
 
-export const MAXTIMEPROCESSING = BatchTranscriptionEnv.MAXTIMEPROCESSING || 3000000;
-export const TIMEBETTWENEACHPOLLING = BatchTranscriptionEnv.TIMEBETTWENEACHPOLLING || 3000;
+export const MAXTIMEPROCESSING = Number(BatchTranscriptionEnv.MAX_TIME_PROCESSING) || 3000000;
+export const TIMEBETTWENEACHPOLLING = Number(BatchTranscriptionEnv.TIME_BETWEEN_EACH_POLLING) || 3000;
 export const API_KEY = BatchTranscriptionEnv.API_KEY || "";
-export const MAXSIZEAUDIO = Number(BatchTranscriptionEnv.MAXSIZEAUDIO) || 15;
+export const MAXSIZEAUDIO = Number(BatchTranscriptionEnv.MAX_SIZE_AUDIO) || 15;
